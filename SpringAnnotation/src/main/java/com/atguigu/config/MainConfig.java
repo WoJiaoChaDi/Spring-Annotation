ab5a54772c54931abe4f3c7a02d46db88705d53b
wojiaochadi <264835229@qq.com>
2019-12-19 17:55:57 +0800

    包扫描 排除部分、包含部分

--- SpringAnnotation/src/main/java/com/atguigu/config/MainConfig.java
+++ SpringAnnotation/src/main/java/com/atguigu/config/MainConfig.java
@@ -4,10 +4,21 @@ import com.atguigu.bean.Person;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
 //配置类 等于 以前的配置文件
 @Configuration
-@ComponentScan(value = "com.atguigu")
+@ComponentScan(
+                //扫描指定的包
+                value = "com.atguigu",
+                //排除部分扫描，排除注解，排除Controller这个注解
+                //excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class, Service.class})}
+                //指定部分扫描，需要关闭默认的  userDefaultFilters
+                useDefaultFilters = false,
+                includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class})}
+                )
 public class MainConfig {
 
     //@Bean默认是以方法名当做 bean 的 id
