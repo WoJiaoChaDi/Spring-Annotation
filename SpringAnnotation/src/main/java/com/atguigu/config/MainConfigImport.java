17c4d09e37bb157a2714f173b3e565d34fd4ff64
wojiaochadi <264835229@qq.com>
2019-12-20 16:04:20 +0800

    @ImportBeanDefinitionRegistr 实现ImportBeanDefinitionRegistrar接口，手工进行bean的注册

--- SpringAnnotation/src/main/java/com/atguigu/config/MainConfigImport.java
+++ SpringAnnotation/src/main/java/com/atguigu/config/MainConfigImport.java
@@ -2,13 +2,14 @@ package com.atguigu.config;
 
 import com.atguigu.bean.Color;
 import com.atguigu.bean.Red;
+import com.atguigu.config.importBeanDefinitionRegistrar.MyImportBeanDefinitionRegistrar;
 import com.atguigu.config.importSelector.MyImportSelector;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 //默认快速导入组件，id默认是组件的全类名com.atguigu.bean.Color
-@Import({Color.class, Red.class, MyImportSelector.class})
+@Import({Color.class, Red.class, MyImportSelector.class, MyImportBeanDefinitionRegistrar.class})
 public class MainConfigImport {
 
 }
