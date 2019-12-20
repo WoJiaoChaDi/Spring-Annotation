864b6f9c00946c1391bc05cbddd5a98180b3625d
wojiaochadi <264835229@qq.com>
2019-12-20 14:57:47 +0800

    单实例的时候，ioc容器启动的时候，创建对象了 多实例的时候，每次获取的时候，创建对象

--- SpringAnnotation/src/main/java/com/atguigu/config/MainConfig2.java
+++ SpringAnnotation/src/main/java/com/atguigu/config/MainConfig2.java
@@ -14,13 +14,13 @@ public class MainConfig2 {
      * @see org.springframework.web.context.WebApplicationContext#SCOPE_REQUEST
      * @see org.springframework.web.context.WebApplicationContext#SCOPE_SESSION
      *
-     * SCOPE_SINGLETON：单实例
-     * SCOPE_PROTOTYPE：多实例
+     * SCOPE_SINGLETON：单实例：ioc容器启动的时候，创建了该对象
+     * SCOPE_PROTOTYPE：多实例：ioc容器获取的时候，创建对象
      * SCOPE_REQUEST：同一次请求一个实例
      * SCOPE_SESSION：同一个session，创建一个实例
      *
      * */
-    //@Scope("prototype")//默认是单实例，这里改成多实例
+    @Scope("prototype")//默认是单实例，这里改成多实例
     @Bean("person")
     public Person person(){
         System.out.println("给容器中添加Person");
