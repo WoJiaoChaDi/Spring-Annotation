6be829326f4f9aff7c1330998532c54e15cb8a8f
wojiaochadi <264835229@qq.com>
2019-12-20 14:49:41 +0800

    Configuration默认是单实例的

---
+++ SpringAnnotation/src/test/java/com/atguigu/test/SpringAnnotationIOCTest2.java
@@ -0,0 +1,26 @@
+package com.atguigu.test;
+
+import com.atguigu.bean.Person;
+import com.atguigu.config.MainConfig;
+import com.atguigu.config.MainConfig2;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringAnnotationIOCTest2 {
+
+    @Test
+    public void test01(){
+        ApplicationContext cxt = new AnnotationConfigApplicationContext(MainConfig2.class);
+        String[] definitionNames = cxt.getBeanDefinitionNames();
+        for (String beanName : definitionNames) {
+            System.out.println(beanName);
+        }
+
+        //默认是单实例的
+        Object bean1 = cxt.getBean(Person.class);
+        Object bean2 = cxt.getBean(Person.class);
+        System.out.println(bean1 == bean2);
+    }
+
+}
