4b2ec84a5829a2b808c9e2d6f881b21a3619f089
wojiaochadi <264835229@qq.com>
2019-12-20 15:56:09 +0800

    @Import({MyImportSelector.class})  实现ImportSelector接口，会导入selectImports返回的数组中，包含的bean的全类名的组件，而不是MyImportSelector类本身

--- SpringAnnotation/src/main/java/com/atguigu/config/MainConfigImport.java
+++ SpringAnnotation/src/main/java/com/atguigu/config/MainConfigImport.java
@@ -2,12 +2,13 @@ package com.atguigu.config;
 
 import com.atguigu.bean.Color;
 import com.atguigu.bean.Red;
+import com.atguigu.config.importSelector.MyImportSelector;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 //默认快速导入组件，id默认是组件的全类名com.atguigu.bean.Color
-@Import({Color.class, Red.class})
+@Import({Color.class, Red.class, MyImportSelector.class})
 public class MainConfigImport {
 
 }
