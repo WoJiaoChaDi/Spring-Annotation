23b52a8a8172bb0896abb4dbd8d66dc384a8ed30
wojiaochadi <264835229@qq.com>
2019-12-23 15:03:41 +0800

    自己创建的对象只会调用构造方法

--- SpringAnnotation/src/test/java/com/atguigu/test/SpringAnnotationIOCOfLiceCycleTest.java
+++ SpringAnnotation/src/test/java/com/atguigu/test/SpringAnnotationIOCOfLiceCycleTest.java
@@ -1,5 +1,6 @@
 package com.atguigu.test;
 
+import com.atguigu.bean.Car;
 import com.atguigu.config.MainConfig;
 import com.atguigu.config.MainConfigOfLifeCycle;
 import org.junit.Test;
@@ -13,6 +14,10 @@ public class SpringAnnotationIOCOfLiceCycleTest {
     public void test01(){
         //1.创建ioc容器
         AnnotationConfigApplicationContext cxt = new AnnotationConfigApplicationContext(MainConfigOfLifeCycle.class);
+
+        System.out.println("========自己创建只会执行构造方法=========");
+        Car car = new Car();
+        System.out.println("========自己创建只会执行构造方法=========");
 
         //容器关闭
         cxt.close();
